06802297ce4f1c867068ac7e0467a5bb0261ba01
Michael <akariasmorum@gmail.com>
2021-08-21 19:09:08 +0300

    models added

--- django_hotels/hotels/models.py
+++ django_hotels/hotels/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    telephone = models.CharField(max_length=15)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
